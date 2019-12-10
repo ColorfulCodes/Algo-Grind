635a9348754f749764de7be231a235b4e3d13afc
colorfulcodes <codescolorful@gmail.com>
2019-12-10 16:16:06 -0500

    feat: Interview question I failed for FAANG

---
+++ Random/nestedWeightSum.py
@@ -0,0 +1,15 @@
+def helper(alist)
+    total = 0
+    
+    def nested(alist, depth):
+        count = 0
+        for i in alist:
+            if i == int():
+                count += i
+            else:
+                nested(i,depth+1)
+                depth-=1
+        return 
+    return total
+    
+helper([4,5,[1,2[3],9],8])
